e4678793f9128cad6f4953e8ce80ced558a22f00
porapospat <145399642+porapospat@users.noreply.github.com>
2024-03-04 20:06:33 +0300

    изменены функции для подсчёта пользователей

--- Polina_DOD/db.py
+++ Polina_DOD/db.py
@@ -20,23 +20,11 @@ async def count_users_stat(start_time, end_time):
     total_users = cursor.fetchone()[0]
     return total_users
 
-async def count_users_December(start_December, end_December):
+async def count_users_month(start_time, end_time):
     cursor.execute('SELECT COUNT(DISTINCT user_id) FROM user_stat WHERE timestamp BETWEEN ? AND ? AND user_id NOT IN (SELECT DISTINCT user_id FROM user_stat WHERE timestamp < ?)',
-                   (start_December, end_December, start_December))
-    users_December = cursor.fetchone()[0]
-    return users_December
-
-async def count_users_January(start_January, end_January):
-    cursor.execute('SELECT COUNT(DISTINCT user_id) FROM user_stat WHERE timestamp BETWEEN ? AND ? AND user_id NOT IN (SELECT DISTINCT user_id FROM user_stat WHERE timestamp < ?)',
-                   (start_January, end_January, start_January))
-    users_January = cursor.fetchone()[0]
-    return users_January
-
-async def count_users_February(start_February, end_February):
-    cursor.execute('SELECT COUNT(DISTINCT user_id) FROM user_stat WHERE timestamp BETWEEN ? AND ? AND user_id NOT IN (SELECT DISTINCT user_id FROM user_stat WHERE timestamp < ?)',
-                   (start_February, end_February, start_February))
-    users_February = cursor.fetchone()[0]
-    return users_February
+                   (start_time, end_time, start_time))
+    users_month = cursor.fetchone()[0]
+    return users_month
 
 
 #функции для подсчета общего количества пользователей за выбранный период времени (за весь период, декабрь, январь и тд)
@@ -45,20 +33,8 @@ async def count_users_url(start_url, end_url):
     total_url = cursor.fetchone()[0]
     return total_url
 
-async def count_users_url_December(start_url_December, end_url_December):
+async def count_users_month_url(start_url, end_url):
     cursor.execute('SELECT COUNT(DISTINCT user_id) FROM url WHERE timestampurl BETWEEN ? AND ? AND user_id NOT IN (SELECT DISTINCT user_id FROM url WHERE timestampurl < ?)',
-                   (start_url_December, end_url_December, start_url_December))
-    users_url_December = cursor.fetchone()[0]
-    return users_url_December
-
-async def count_users_url_January(start_url_January, end_url_January):
-    cursor.execute('SELECT COUNT(DISTINCT user_id) FROM url WHERE timestampurl BETWEEN ? AND ? AND user_id NOT IN (SELECT DISTINCT user_id FROM url WHERE timestampurl < ?)',
-                   (start_url_January, end_url_January, start_url_January))
-    users_url_January = cursor.fetchone()[0]
-    return users_url_January
-
-async def count_users_url_February(start_url_February, end_url_February):
-    cursor.execute('SELECT COUNT(DISTINCT user_id) FROM url WHERE timestampurl BETWEEN ? AND ? AND user_id NOT IN (SELECT DISTINCT user_id FROM url WHERE timestampurl < ?)',
-                   (start_url_February, end_url_February, start_url_February))
-    users_url_February = cursor.fetchone()[0]
-    return users_url_February
+                   (start_url, end_url, start_url))
+    users_month_url = cursor.fetchone()[0]
+    return users_month_url
