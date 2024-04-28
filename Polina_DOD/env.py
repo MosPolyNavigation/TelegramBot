1f98207a4b710f14d581552959b22dab718ef0b7
Easton <eastongalaxy@gmail.com>
2024-04-28 19:46:54 +0300

    fix statistics in dod & poll

--- Polina_DOD/env.py
+++ Polina_DOD/env.py
@@ -1,9 +1,23 @@
+from datetime import datetime
 import sqlite3
 import json
 import os
 
 from aiogram import Bot
 from aiogram.enums.parse_mode import ParseMode
+
+
+def adapt_datetime(ts):
+    return ts.isoformat(" ")
+
+
+def convert_datetime(ts):
+    return datetime.strptime(ts, "%Y-%m-%d %H:%M:%S")
+
+
+sqlite3.register_adapter(datetime, adapt_datetime)
+sqlite3.register_converter("timestamp", convert_datetime)
+
 # подключение к базе данных
 conn = sqlite3.connect('my.db')
 cursor = conn.cursor()
@@ -16,8 +30,7 @@ if not TOKEN:
 
 bot = Bot(token=TOKEN, parse_mode=ParseMode.HTML)
 
-
 # проголосовавшие пользователи
 voted_users = []
 
-poll_results:dict = json.loads(open('./poll.json').read())
+poll_results: dict = json.loads(open('./poll.json').read())
--- Polina_DOD/main.py
+++ Polina_DOD/main.py
@@ -2,12 +2,13 @@ import asyncio
 import logging
 from datetime import datetime
 
-from aiogram import Dispatcher
+from aiogram import types, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.fsm.strategy import FSMStrategy
 
 from env import *
 from handlers import basic_router
+
 
 async def main():
     logging.basicConfig(
@@ -16,14 +17,12 @@ async def main():
     dp = Dispatcher(storage=MemoryStorage(), fsm_strategy=FSMStrategy.CHAT)
     dp.include_router(basic_router)
 
-    poll_waiting = asyncio.create_task(send_poll())
-    poll_waiting
+    await asyncio.create_task(send_poll())
+    await asyncio.create_task(send_dod())
 
-    file_sending = asyncio.create_task(send_dod())
-    file_sending
-    
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
+
 
 # отправка программы ДОД
 async def send_dod():
@@ -33,7 +32,7 @@ async def send_dod():
     # ожидание до момента отправки опроса
     while datetime.now() < scheduled_time:
         await asyncio.sleep(60)  # проверка каждую минуту
-    
+
     # получение списка всех пользователей
     users = await get_all_users()
     file_pdf = 'Программа Дня открытых дверей.pdf'
@@ -47,42 +46,45 @@ async def send_dod():
 # отправка опроса в определенный день и время
 async def send_poll():
     # день, месяц, год и время отправки опроса
-    scheduled_time = datetime(2024, 3, 23, 18, 0, 0)
+    scheduled_time = datetime(2025, 3, 23, 18, 0, 0)
 
     # ожидание до момента отправки опроса
     while datetime.now() < scheduled_time:
         await asyncio.sleep(60)  # проверка каждую минуту
 
     # получение списка пользователей, которые воспользовались командой старт в заданный период
-    start_time = '2024-03-01 00:00:00'
-    end_time = '2024-03-21 23:59:59'
+    start_time = datetime(2025, 3, 1, 0, 0, 0)
+    end_time = datetime(2025, 3, 21, 23, 59, 59)
     users = await get_users_in_period(start_time, end_time)
 
     # отправка опроса каждому пользователю из списка
     for user_id in users:
-            if user_id in voted_users: continue
-            await bot.send_poll(chat_id = user_id,
-                                    question = 'День открытых дверей окончен.\nПожалуйста, оцените работу бота', 
-                                    options = ['Отлично',
-                                         'Хорошо',
-                                         'Удовлетворительно', 
-                                         'Плохо',
-                                         'Ужасно'],
-                                    is_anonymous = False,
-                                    type = 'regular',
-                                    allows_multiple_answers = False)
+        if user_id in voted_users: continue
+        await bot.send_poll(chat_id=user_id,
+                            question='День открытых дверей окончен.\nПожалуйста, оцените работу бота',
+                            options=['Отлично',
+                                     'Хорошо',
+                                     'Удовлетворительно',
+                                     'Плохо',
+                                     'Ужасно'],
+                            is_anonymous=False,
+                            type='regular',
+                            allows_multiple_answers=False)
 
-# Получение полного списка пользователей за весь период времени 
+
+# Получение полного списка пользователей за весь период времени
 async def get_all_users():
     cursor.execute('SELECT DISTINCT user_id FROM user_stat')
     users = [row[0] for row in cursor.fetchall()]
     return users
+
 
 # Функция для получения списка пользователей, воспользовавшихся командой старт в заданный период
 async def get_users_in_period(start_time, end_time):
     cursor.execute('SELECT DISTINCT user_id FROM user_stat WHERE timestamp BETWEEN ? AND ?', (start_time, end_time))
     users = [row[0] for row in cursor.fetchall()]
     return users
+
 
 # Запуск бота
 if __name__ == "__main__":
